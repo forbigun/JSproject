8dde1da836e2a8552e98db577b8b0f83a50cc45e
Aleksandr Khabibullin <alexander.habibullin@yandex.ru>
2021-07-20 17:42:24 +0300

    Added first exercise

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
 'use strict';
 
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-console.log(typeof (null));
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
 
+let lastFilm = prompt('Один из последних просмотренных фильмов?', '');
+let lastFilmRating = +prompt('Как вы его оцените?', '');
+
+personalMovieDB.movies[lastFilm] = lastFilmRating;
+
+console.log(personalMovieDB);
 
 
 
