46df7c637072cf52789cc58c09d263e140aa25c0
Aleksandr Khabibullin <alexander.habibullin@yandex.ru>
2021-07-26 18:00:10 +0300

    Clean code

--- js/script.js
+++ js/script.js
@@ -1,36 +1,6 @@
 'use strict';
-let isCycleEnded;
-
-const personalMovieDB = {
-    count: 0,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-};
-
-do {
-
-    let lastFilm = prompt('Один из последних просмотренных фильмов?', '');
-    if (lastFilm.length === 0 || !lastFilm.trim()) {
-        continue;
-    }
-    let lastFilmRating = +prompt('Как вы его оцените?', '');
-
-    personalMovieDB.count += 1;
-
-    personalMovieDB.movies[lastFilm] = lastFilmRating;
-
-    isCycleEnded = confirm("Вы закончили?");
-}
-while (!isCycleEnded);
-
-console.log(personalMovieDB);
 
 
-
-
-
-
+console.log('Pog');
 
 
