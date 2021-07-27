5b42c4c49d8d5cb6e6ddfa04f71a4138f9a5ed29
Aleksandr Khabibullin <alexander.habibullin@yandex.ru>
2021-07-27 22:03:02 +0300

    Add random tasks

--- js/script.js
+++ js/script.js
@@ -1,36 +1,51 @@
 'use strict';
-let isCycleEnded;
 
-const personalMovieDB = {
-    count: 0,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-};
-
-do {
-
-    let lastFilm = prompt('Один из последних просмотренных фильмов?', '');
-    if (lastFilm.length === 0 || !lastFilm.trim()) {
-        continue;
+function reverse(number) {
+    const strNumber = String(number);
+    let reverseNumber = '';
+    const end = strNumber.charAt(0) === '-' ? 1 : 0;
+    for (let i = strNumber.length - 1; i >= end; i--) {
+        reverseNumber += strNumber[i];
     }
-    let lastFilmRating = +prompt('Как вы его оцените?', '');
-
-    personalMovieDB.count += 1;
-
-    personalMovieDB.movies[lastFilm] = lastFilmRating;
-
-    isCycleEnded = confirm("Вы закончили?");
+    return end === 1 ? +reverseNumber * -1 : +reverseNumber;
 }
-while (!isCycleEnded);
-
-console.log(personalMovieDB);
 
 
+function walk(array) {
+    if (array.length !== 10) {
+        return false;
+    }
+
+    const direction = {
+        'n': 's',
+        's': 'n',
+        'w': 'e',
+        'e': 'w'
+    };
+
+    const trip = Array.prototype.splice.call([].concat(array), 0, 5);
+
+    const returnTrip = Array.prototype.splice.call([].concat(array), 5);
+
+    for (let i = 0; i < trip.length; i++) {
+        if (direction[trip[i]] !== returnTrip[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 
-
-
+let maxProfit = function (prices) {
+    let max = 0;
+    for (let i = 0; i < prices.length - 1; i++) {
+        for (let j = i + 1; j < prices.length; j++) {
+            if (prices[j] - prices[i] > max) {
+                max = prices[j] - prices[i];
+            }
+        }
+    }
+    return max;
+};
 
 
