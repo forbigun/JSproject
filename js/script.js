ca4b94dd319f6f0c796e8bac4b007579da0d93bb
Aleksandr Khabibullin <alexander.habibullin@yandex.ru>
2021-07-29 23:16:59 +0300

    add counter with reset

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 'use strict';
 
+const btns = document.querySelectorAll('button');
+function makeCounter() {
+    let i = 0;
+    return function () {
+        console.log(i++);
+    };
+}
+
+let counter = makeCounter();
+
+function resetCounter() {
+    btns[0].removeEventListener('mouseenter', counter);
+    counter = makeCounter();
+    btns[0].addEventListener('mouseenter', counter);
+}
+
+btns[0].addEventListener('mouseenter', counter);
+btns[1].addEventListener('click', resetCounter);
 
 
 
